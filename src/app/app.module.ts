c5e7a9dc0274cb08d411f0bcfade6e4eda5a7762
jimjones26 <jimjones26@gmail.com>
2017-03-12 16:32:32 -0400

    generate top level components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,16 @@ import { AppComponent } from './app.component';
 import { MessagesService } from './_services/messages.service';
 import { ThreadsService } from './_services/threads.service';
 import { UserService } from './_services/user.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ChatThreadsComponent } from './chat-threads/chat-threads.component';
+import { ChatWindowComponent } from './chat-window/chat-window.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ChatThreadsComponent,
+    ChatWindowComponent
   ],
   imports: [
     BrowserModule,
